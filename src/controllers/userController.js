b5d70eca6ab4e43caf8354c831c7335e49090b55
Hector Vega <hectormartinez1vg@gmail.com>
2024-05-10 23:49:32 -0600

    add base to work

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,18 @@
 
-export const indexPrueba = (req, res) => {
+export const index = (req, res) => {
     res.render('auth/index' , {
         title : 'Prueba Ejs'
+    })
+}
+
+export const aboutUs = (req, res) => {
+    res.render('auth/aboutUs' , {
+        title : 'Sobre Nosotros'
+    })
+}
+
+export const binarySearch = (req, res) => {
+    res.render('auth/searchAlgorithms/binarySearch' , {
+        title : 'Busqueda Binaria'
     })
 }
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,9 +1,11 @@
 import express from "express"
-import { indexPrueba } from '../controllers/userController.js';
+import { index, aboutUs , binarySearch} from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
-userRouter.get('/index', indexPrueba);
+userRouter.get('/index', index);
+userRouter.get('/aboutUs', aboutUs);
+userRouter.get('/searchAlgorithms/binarySearch', binarySearch);
 
 
 export default userRouter
